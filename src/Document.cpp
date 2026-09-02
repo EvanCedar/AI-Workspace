427cbff5baee7668e5b874acb401cb7dc45aa554
Evan Cedar <evanc05@vt.edu>
2026-09-02 16:30:21 -0400

    Implement Prompt Message and Document classes

--- src/Document.cpp
+++ src/Document.cpp
@@ -1,51 +1,85 @@
 #include "Document.hpp"
 
-Document::Document(std::string title, std::string contents) {
-    // TODO: implement according to the M1 specification.
+#include <fstream>
+#include <sstream>
+
+Document::Document() : title_(""), contents_(""), sourcePath_("") {
+    
+    // nothing.
+}
+
+Document::Document(std::string title, std::string contents) : title_(std::move(title)), contents_(std::move(contents)) {
+    
+    // nothing.
 
 }
 
 bool Document::operator==(const Document& other) const {
-    // TODO
+    return title_ == other.title_ &&  sourcePath_ == other.sourcePath_ && contents_ == other.contents_;
 
 }
 
 bool Document::operator!=(const Document& other) const {
-    // TODO
-
+    return !(*this == other);
 }
 
-bool Document::load(const std::string& path) {
-    // TODO
 
+
+bool Document::load(const std::string& path) {
+    std::ifstream file(path);
+
+    if (!file.is_open()) {
+        return false;
+    }
+    std::ostringstream buffer;
+    buffer << file.rdbuf();
+
+    if(file.bad()) {
+        return false;
+    }
+
+    std::string newContents = buffer.str();
+    std::size_t lastSlash = path.find_last_of("/\\");
+    std::string newTitle;
+
+    if (lastSlash == std::string::npos) {
+        newTitle = path;
+    } else {
+        newTitle = path.substr(lastSlash + 1);
+    }
+
+    sourcePath_ = path;
+    title_ = std::move(newTitle);
+    contents_ = std::move(newContents);
+    return true;
 }
 
 const std::string& Document::title() const noexcept {
-    // TODO
+    return title_;
 
 }
 
 const std::string& Document::sourcePath() const noexcept {
-    // TODO
+    return sourcePath_;
 
 }
 
 const std::string& Document::contents() const noexcept {
-    // TODO
+    return contents_;
   
 }
 
 void Document::setTitle(std::string title) {
-    // TODO
+    title_ = std::move(title);
   
 }
 
 std::size_t Document::characterCount() const noexcept {
-    // TODO
+    return contents_.size();
     
 }
 
 bool Document::empty() const noexcept {
-    // TODO
+    return contents_.empty();
    
 }
